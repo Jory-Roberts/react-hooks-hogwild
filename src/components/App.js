f61799949d1f8f6292c5bf22f83acd95360fe611
Jory Roberts <robertsjory13@gmail.com>
2023-06-01 02:05:05 -0500

    changes formatting

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import Nav from './Nav';
-import HogList from './HogList';
-
-import hogs from '../porkers_data';
+import React from "react";
+import Nav from "./Nav";
+import HogList from "./HogList";
+import hogs from "../porkers_data";
 
 console.log(hogs);
 
 function App() {
-	return (
-		<div className='App'>
-			<Nav />
-			<HogList hogs={hogs} />
-		</div>
-	);
+    return (
+        <div className="App">
+            <Nav />
+            <HogList hogs={hogs} />
+        </div>
+    );
 }
 
 export default App;
--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,18 +1,18 @@
 function Tile({ handleIsGreased, isGreased }) {
-	return (
-		<div className='tile-container'>
-			<p>Sort by</p>
-			<select>
-				<option value='name'>Name</option>
-				<option value='weight'>Weight</option>
-			</select>
-			<input
-				onChange={handleIsGreased}
-				type='checkbox'
-				checked={isGreased}
-			/>
-			<label>Greased Pigs</label>
-		</div>
-	);
+    return (
+        <div className='tile-container'>
+            <p>Sort by</p>
+            <select>
+                <option value='name'>Name</option>
+                <option value='weight'>Weight</option>
+            </select>
+            <input
+                onChange={handleIsGreased}
+                type='checkbox'
+                checked={isGreased}
+            />
+            <label>Greased Pigs</label>
+        </div>
+    );
 }
 export default Tile;
